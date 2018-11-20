eb01753d4c293126cff4ef7b06390c36e1b6316f
UnknownWellFail <eugene.barannik37@gmail.com>
2018-11-20 15:55:16 +0300

    Security with methods

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -22,12 +22,7 @@ class App extends Component {
         // Find the text field via the React ref
         const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
 
-        Tasks.insert({
-            text,
-            createdAt: new Date(), // current time
-            owner: Meteor.userId(),  // id of logged user
-            username: Meteor.user().username, //username of logged user
-        });
+        Meteor.call('tasks.insert', text);
 
         // Clear form
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
