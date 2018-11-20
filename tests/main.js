b816bcd90390ecd10cedceb827eea7dbcbd29376
UnknownWellFail <eugene.barannik37@gmail.com>
2018-11-20 18:16:25 +0300

    Testing

--- tests/main.js
+++ tests/main.js
@@ -1,20 +1,43 @@
-import assert from "assert";
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import { assert } from 'chai';
+
+import { Tasks } from '../imports/api/tasks.js';
+
 
 describe("simple-todos", function () {
-  it("package.json has correct name", async function () {
-    const { name } = await import("../package.json");
-    assert.strictEqual(name, "simple-todos");
-  });
-
-  if (Meteor.isClient) {
-    it("client is not server", function () {
-      assert.strictEqual(Meteor.isServer, false);
-    });
-  }
 
   if (Meteor.isServer) {
-    it("server is not client", function () {
-      assert.strictEqual(Meteor.isClient, false);
+    describe('Tasks', () => {
+      describe('methods', () => {
+        const userId = Random.id();
+        let taskId;
+  
+        beforeEach(() => {
+          Tasks.remove({});
+          taskId = Tasks.insert({
+            text: 'test task',
+            createdAt: new Date(),
+            owner: userId,
+            username: 'tmeasday',
+          });
+        });
+  
+        it('can delete owned task', () => {
+          // Find the internal implementation of the task method so we can
+          // test it in isolation
+          const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+   
+          // Set up a fake method invocation that looks like what the method expects
+          const invocation = { userId };
+   
+          // Run the method with `this` set to the fake invocation
+          deleteTask.apply(invocation, [taskId]);
+   
+          // Verify that the method does what we expected
+          assert.equal(Tasks.find().count(), 0);
+        });
+      });
     });
   }
 });
