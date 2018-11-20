9762f1f3885f0e07e2f9c4a12ade225f69bbf9e5
UnknownWellFail <eugene.barannik37@gmail.com>
2018-11-20 14:39:06 +0300

    Storing tasks in a collection

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+ 
+import { Tasks } from '../api/tasks.js';
  
 import Task from './Task.js';
- 
 // App component - represents the whole app
-export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
-  }
+class App extends Component {
  
   renderTasks() {
-    return this.getTasks().map((task) => (
-      <Task key={task._id} task={task} />
-    ));
+    return this.props.tasks.map((task) => (
+        <Task key={task._id} task={task} />
+      ));
   }
  
   render() {
@@ -32,3 +27,8 @@ export default class App extends Component {
     );
   }
 }
+export default withTracker(() => {
+    return {
+      tasks: Tasks.find({}).fetch(),
+    };
+  })(App);
