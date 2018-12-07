e0f350991367b9adb645bb9557c710697cf28990
UnknownWellFail <eugene.barannik37@gmail.com>
2018-12-07 17:48:26 +0300

    Add cron with sending email

--- server/main.js
+++ server/main.js
@@ -1,13 +1,23 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { SyncedCron } from 'meteor/percolate:synced-cron';
 
 import '../imports/api/tasks.js';
 import '../imports/api/lists.js';
 import { Lists } from '../imports/api/lists.js';
+import { addStatCron } from './cron/stat-cron';
+
+Meteor.startup( ()=> {
+  process.env.MAIL_URL = "smtp://mctestemail@yandex.ru:mctestemailpassword@smtp.yandex.ru:587/";
+  addStatCron();
+  SyncedCron.start();
+});
 
 if (Meteor.isServer) {
   Accounts.onCreateUser( (options, user) => {
     if (user.services.google) {
       user.username = user.services.google.email;
+      user.email = user.services.google.email;
     }
     Lists.insert({
       name: 'My Tasks',
